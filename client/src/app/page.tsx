83f260f6505f63c4da7fd514255a986e58919119
MolinDeng <zjumorning@gmail.com>
2024-02-14 16:48:04 -0500

    fea:convans grainy bg

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,14 @@
-import RoughCanvas from '@/components/RoughCanvas';
+// import RoughCanvas from '@/components/RoughCanvas';
+import dynamic from 'next/dynamic'
+ 
+const RoughCanvasNoSSR = dynamic(() => import('@/components/RoughCanvas'), {
+  ssr: false,
+})
 
 export default function Home() {
   return (
     <main className="h-screen w-screen">
-      <RoughCanvas />
+      <RoughCanvasNoSSR />
     </main>
   );
 }
--- client/src/components/RoughCanvas.tsx
+++ client/src/components/RoughCanvas.tsx
@@ -1,13 +1,17 @@
 'use client';
-import { FC } from 'react';
-import { use, useEffect, useLayoutEffect, useRef } from 'react';
+
+import { createGrainyBg } from '@/lib/canvas-utils';
+import { FC, useEffect, useRef, useState } from 'react';
 import rough from 'roughjs';
+import { Drawable } from 'roughjs/bin/core';
 
 const generator = rough.generator();
 interface RoughCanvasProps {}
 
 const RoughCanvas: FC<RoughCanvasProps> = ({}) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [drawing, setDrawing] = useState<boolean>(false);
+  const [elements, setElements] = useState<any[]>([]); // * May need to change type to Drawable[]
 
   // set canvas size to window size
   useEffect(() => {
@@ -21,26 +25,28 @@ const RoughCanvas: FC<RoughCanvasProps> = ({}) => {
     const ctx = canvasRef.current.getContext('2d');
     if (!ctx) return;
     // * Debug only fill canvas with light blue
+
     // ctx.fillStyle = '#e6f1ff';
-    // const { width, height } = canvasRef.current!.getBoundingClientRect();
+    // const { width, height } = ctx.canvas.getBoundingClientRect();
     // ctx.fillRect(0, 0, width, height);
     // * Debug only end
-
+    createGrainyBg(ctx);
     const roughCanvas = rough.canvas(canvasRef.current);
-    const rect = generator.rectangle(10, 10, 300, 200);
-    const line = generator.line(80, 10, 10, 100);
+    const rect: Drawable = generator.rectangle(10, 10, 300, 200);
+    const line: Drawable = generator.line(80, 10, 10, 100);
     roughCanvas.draw(rect);
     roughCanvas.draw(line);
   }, [canvasRef]);
 
-  const handleMouseDown = () => {
-    console.log('mouse down');
+  const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    setDrawing(true);
   };
-  const handleMouseMove = () => {
-    console.log('mouse move');
+  const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!drawing) return;
+    console.log('mouse move' + event.clientX + ' ' + event.clientY);
   };
-  const handleMouseUp = () => {
-    console.log('mouse up');
+  const handleMouseUp = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    setDrawing(false);
   };
   return (
     <canvas
